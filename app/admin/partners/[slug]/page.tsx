78af12452d9d962a75159d2e176113885f7815e0
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-09-09 22:49:08 +0330

    Load partner editor from database

--- app/admin/partners/[slug]/page.tsx
+++ app/admin/partners/[slug]/page.tsx
@@ -1,6 +1,12 @@
+import { notFound } from "next/navigation";
 import { CmsPartnerEditor } from "../../../../components/cms-partner-editor";
+import { db } from "../../../../lib/db";
+
+export const dynamic = "force-dynamic";
 
 export default async function EditPartnerPage({ params }: { params: Promise<{ slug: string }> }) {
-  await params;
-  return <CmsPartnerEditor mode="edit" />;
+  const { slug } = await params;
+  const partner = await db.partner.findFirst({ where: { slug, deletedAt: null } });
+  if (!partner) notFound();
+  return <CmsPartnerEditor mode="edit" partner={partner} />;
 }
